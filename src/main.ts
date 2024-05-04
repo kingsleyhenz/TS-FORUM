0090f369f7d3a13db78c2a8e5272ecfe48febe39
BLACKHENZ29 <ayehenz29@gmail.com>
2024-05-04 20:33:09 +0100

    Established mongodb connection

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
+import * as dotenv from "dotenv"
+dotenv.config();
 import express from "express";
+import { json, urlencoded } from "body-parser";
+import mongoose from "mongoose";
+
 
 const app = express();
 
-app.listen(5050,()=> console.log("Server is Active on port 5050"));
+app.use(urlencoded({
+    extended: true
+}));
+
+app.use(json());
+
+const start = async ()=>{
+    if(!process.env.DB_URI) throw new Error("DB_URI is required");
+    
+    try {
+        await mongoose.connect(process.env.DB_URI)
+    } catch (error) {
+        throw new Error("Database Error")
+    }
+    app.listen(5050,()=> console.log("Server is Active on port 5050"));
+}
+
+
+start();
